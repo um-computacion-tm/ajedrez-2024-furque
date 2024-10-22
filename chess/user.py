7eb6e5e4a94e9abde9171c9cf5ef44468ddf275c
furque <joaquinfurque8@gmail.com>
2024-10-22 12:54:53 -0300

    FINAL FIXES

--- chess/user.py
+++ chess/user.py
@@ -4,35 +4,35 @@ class user:
     def __init__(self, board):
         self.board = board
 
-    # Verifica si el jugador tiene un Rey en el tablero
+    # Comprueba si el jugador tiene un Rey en el tablero
     def has_king(self, color):
-        return self.has_piece(color, King)  # Llama a has_piece para buscar al Rey
+        return self.has_piece(color, King)  
 
-    # Verifica si un jugador tiene una pieza de un tipo específico en el tablero
+    # Comprueba si un jugador tiene una pieza de un tipo específico en el tablero
     def has_piece(self, color, piece_type):
-        for row in self.board.board:  # Recorre cada fila del tablero
+        for row in self.board.board:  
             if self.has_piece_in_row(row, color, piece_type):  
                 return True
-        return False  #Si no encuentra la pieza devuelve False
+        return False  
 
-    # Verifica si una fila contiene una pieza del tipo y color especificado
+    # Comprueba si una fila contiene una pieza del tipo y color especificado
     def has_piece_in_row(self, row, color, piece_type):
         for piece in row:  
             if self.is_piece_of_color(piece, color, piece_type):
                 return True
         return False
 
-    # Verifica si un jugador tiene cualquier pieza en el tablero
+    # Comprueba si un jugador tiene cualquier pieza en el tablero
     def has_pieces(self, color):
         for row in self.board.board:  
             if self.row_has_piece_of_color(row, color):
                 return True
         return False
 
-    # Verifica si una fila tiene piezas del color especificado
+    # Comprueba si una fila tiene piezas del color especificado
     def row_has_piece_of_color(self, row, color):
         for piece in row:  
-            if piece and piece.get_color() == color:  # Verifica si la pieza es del color correcto
+            if piece and piece.get_color() == color:  
                 return True
         return False
 
